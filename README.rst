9d3bfecb593d78affcdcb5ee2d75295df78dc94c
Egawati Panjei <egawati.panjei@gmail.com>
2019-01-16 16:30:28 +0700

    initial commit

---
+++ README.rst
@@ -0,0 +1,9 @@
+===========
+ nti.batch_util
+===========
+
+.. image:: https://travis-ci.org/NextThought/nti.batch_util.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.batch_util
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.batch_util/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.batch_util?branch=master
---
+++ setup.py
@@ -0,0 +1,68 @@
+import codecs
+from setuptools import setup, find_packages
+
+entry_points = {
+    'console_scripts': [
+    ],
+}
+
+
+TESTS_REQUIRE = [
+    'six',
+    'nti.testing',
+    'zope.dottedname',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='nti.batch_util',
+    version=_read('version.txt').strip(),
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI Batch Util",
+    long_description=(_read('README.rst') + '\n\n' + _read('CHANGES.rst')),
+    license='Apache',
+    keywords='legacy batch_util',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    url="https://github.com/NextThought/nti.batch_util",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'Babel',
+        'lingua',
+        'zope.component',
+        'zope.deferredimport',
+        'zope.deprecation',
+        'zope.interface',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ]
+    },
+    entry_points=entry_points,
+)
